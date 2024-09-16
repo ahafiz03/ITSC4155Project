a73204dec80dc622315a52c18f620dc61512a698
unknown <anumhafiz177@gmail.com>
2024-09-15 20:41:18 -0400

    updated tests

--- tests/test_my_module.py
+++ tests/test_my_module.py
@@ -2,10 +2,9 @@ import sys
 import os
 import unittest
 
-# Add the parent directory to the system path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from app import app  # Import the app module
+from app import app
 
 class BasicTestCase(unittest.TestCase):
     def setUp(self):
