5bbd11f374c4ff6f3e673da386eaf85261b676ab
unknown <anumhafiz177@gmail.com>
2024-09-15 20:29:57 -0400

    sample app

--- tests/test_my_module.py
+++ tests/test_my_module.py
@@ -1,4 +1,21 @@
+import sys
+import os
 import unittest
 
-class MyTestCase(unittest.TestCase):
-    pass
+# Add the parent directory to the system path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app import app  # Import the app module
+
+class BasicTestCase(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    def test_index(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Sample App welcome!', response.data)
+
+if __name__ == '__main__':
+    unittest.main()
